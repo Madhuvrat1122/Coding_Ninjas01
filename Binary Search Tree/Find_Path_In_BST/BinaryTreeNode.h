93033e26d8db8b5886fc7876ec4cb1d227b2fc0b
Technical_Rithm <50729418+Madhuvrat1122@users.noreply.github.com>
2024-07-23 13:26:46 +0530

    Create BinaryTreeNode.h

---
+++ Binary Search Tree/Find_Path_In_BST/BinaryTreeNode.h
@@ -0,0 +1,19 @@
+
+template<typename T>
+class BinaryTreeNode {
+    public:
+        T data;
+        BinaryTreeNode<T>* left;
+        BinaryTreeNode<T>* right;
+        // Contructor
+        BinaryTreeNode(T data){
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+        // Destructor
+        ~BinaryTreeNode(){
+            delete left;
+            delete right;
+        }
+};
