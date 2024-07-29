67451f8968d0132f703c4c9ce63c390e8e643e90
Technical_Rithm <50729418+Madhuvrat1122@users.noreply.github.com>
2024-07-29 11:03:27 +0530

    Update BinarySearchTree.h

--- Binary Search Tree/BST_Class_Insert_Delete_Search/BinarySearchTree.h
+++ Binary Search Tree/BST_Class_Insert_Delete_Search/BinarySearchTree.h
@@ -1,4 +1,5 @@
 #include "BinaryTreeNode.h"
+using namespace std;
 class BST {
     BinaryTreeNode<int>* root;
     
@@ -17,13 +18,102 @@ class BST {
         }
     }
     
+    BinaryTreeNode<int>* insertDataHelper(BinaryTreeNode<int>* root, int data){
+        if(root == NULL){
+            BinaryTreeNode<int>* newNode = new BinaryTreeNode<int>(data);
+            return newNode;
+        }
+        // left side
+        else if(root->data > data){
+            root->left = insertDataHelper(root->left, data);
+        }
+        // right side
+        else {
+            root->right = insertDataHelper(root->right, data);
+        }
+        return root;
+    }
+    
+    BinaryTreeNode<int>* deleteDataHelper(int data, BinaryTreeNode<int>* root){
+        // empty tree
+        if(root == NULL){
+            return NULL;
+        }
+        // traverse in left
+        else if(root->data > data){
+            root->left = deleteDataHelper(data, root->left);
+            return root;
+        }
+        // traverse in right
+        else if(root->data < data){
+            root->right = deleteDataHelper(data, root->right);
+            return root;
+        }
+        else {
+            // leaf node
+            if(root->left == NULL && root->right == NULL){
+                delete root;
+                return NULL;
+            }
+            // only left child 
+            else if(root->right == NULL){
+                BinaryTreeNode<int>* temp = root->left;
+                root->left = NULL;
+                delete root;
+                return temp;
+            }
+            // only right child
+            else if(root->left == NULL){
+                BinaryTreeNode<int>* temp = root->right;
+                root->right = NULL;
+                delete root;
+                return temp;
+            }
+            //both child exist
+            // Approach:- replace root with minimum of right child OR replace root with maximum of left child
+            else {
+                // find minimum of right
+                BinaryTreeNode<int>* minNode = root->right;
+                while(minNode->left != NULL){
+                    minNode = minNode->left;
+                }
+                int minData = minNode->data;
+                root->data = minData;
+                root->right = deleteDataHelper(minData, root->right);
+                return root;
+            }
+        }
+        
+    }
+    
+    void printTreeHelper(BinaryTreeNode<int>* root){
+        if(root == NULL){
+            return;
+        }
+        cout<<root->data<<" : ";
+        if(root->left){
+            cout<<"L"<<root->left->data<<",";
+        }
+        if(root->right){
+            cout<<"R"<<root->right->data;
+        }
+        cout<<endl;
+        
+        printTreeHelper(root->left);
+        printTreeHelper(root->right);
+    }
+    
     public:
         BST(){
             root = NULL;
         }
         
         void insertData(int data){
-            
+            root = insertDataHelper(root, data);
+        }
+        
+        void printTree(){
+            printTreeHelper(root);
         }
         
         bool hasData(int data){
@@ -31,10 +121,10 @@ class BST {
         }
         
         void deleteData(int data){
-            
+            root = deleteDataHelper(data, root);
         }
         
         ~BST(){
             delete root;
         }
-}
+};
