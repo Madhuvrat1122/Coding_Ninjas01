438ca83c42a0fdcbf10c4b838d5d246e7d37ede4
Technical_Rithm <50729418+Madhuvrat1122@users.noreply.github.com>
2024-07-29 11:03:48 +0530

    Update main.cpp

--- Binary Search Tree/BST_Class_Insert_Delete_Search/main.cpp
+++ Binary Search Tree/BST_Class_Insert_Delete_Search/main.cpp
@@ -1,1 +1,44 @@
 
+#include <iostream>
+#include "BinarySearchTree.h"
+using namespace std;
+int main()
+{
+    BST b;
+    b.insertData(50);
+    b.insertData(45);
+    b.insertData(58);
+    b.insertData(42);
+    b.insertData(48);
+    b.insertData(55);
+    b.insertData(60);
+    b.insertData(54);
+    b.insertData(56);
+    b.insertData(59);
+    b.insertData(65);
+    b.insertData(57);
+    
+    b.printTree();
+    
+    //delete 57
+    // cout<<"After Delete 57:- "<<endl;
+    // b.deleteData(57);
+    // b.printTree();
+    
+    //delete 57
+    // cout<<"After Delete 56:- "<<endl;
+    // b.deleteData(56);
+    // b.printTree();
+    
+    //delete 55
+    cout<<"After Delete 55:- "<<endl;
+    b.deleteData(55);
+    b.printTree();
+
+    return 0;
+}
+
+
+
+
+
