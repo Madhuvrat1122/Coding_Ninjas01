72b7555f9d6b7825a322dd454055935ee3f60000
Technical_Rithm <50729418+Madhuvrat1122@users.noreply.github.com>
2024-08-02 12:09:17 +0530

    Create PriorityQueue.h

---
+++ Priority Queue/Max_Heap(Max Priority Queue)/PriorityQueue.h
@@ -0,0 +1,74 @@
+#include<vector>
+using namespace std;
+// It is also same as heap sort
+// Time complexity = O(nlogn)
+// Space complexity = O(n)
+// implementing Max-Heap
+class PriorityQueue {
+  vector<int> pq;
+  
+  public:
+    bool isEmpty(){
+        return pq.size()==0;
+    }
+    int getSize(){
+        return pq.size();
+    }
+    int getMax(){
+        if(isEmpty()){
+            return -1;
+        }
+        return pq[0];
+    }
+    void insert(int data){
+        pq.push_back(data);
+        // up heapify
+        int childIndex = pq.size()-1;
+        while(childIndex > 0){
+            int parentIndex = (childIndex-1)/2;
+            if(pq[childIndex] > pq[parentIndex]){
+                int temp = pq[childIndex];
+                pq[childIndex] = pq[parentIndex];
+                pq[parentIndex] = temp;
+                childIndex = parentIndex;
+            }
+            else{
+                break;
+            }
+        }
+    }
+    int deleteMax(){
+        if(isEmpty()){
+            return 0;
+        }
+        int ans = pq[0];
+        pq[0] = pq[pq.size()-1]; // copying the last element with root
+        pq.pop_back(); // removing the last element
+        // down-heapify
+       int parentIndex = 0;
+       int leftIndex = 2*parentIndex + 1;
+       int rightIndex = 2*parentIndex + 2;
+       while(leftIndex < pq.size()){
+           int minIndex = parentIndex;
+           if(pq[leftIndex] > pq[minIndex]){
+               minIndex = leftIndex;
+           }
+           if(rightIndex < pq.size() && pq[rightIndex] > pq[minIndex]){
+               minIndex = rightIndex;
+           }
+           if(minIndex == parentIndex){
+               break;
+           }
+
+           int temp = pq[minIndex];
+           pq[minIndex] = pq[parentIndex];
+           pq[parentIndex] = temp;
+           
+            parentIndex = minIndex;
+            leftIndex = 2*parentIndex + 1;
+            rightIndex = 2*parentIndex + 2;
+       }
+
+        return ans;
+    }
+};
