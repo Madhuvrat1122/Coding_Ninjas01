678e8120b0af91820d5c6440020cbb47aad6f3a8
Technical_Rithm <50729418+Madhuvrat1122@users.noreply.github.com>
2024-08-18 10:08:36 +0530

    Create TrieNode.h

---
+++ Tries and Huffman Coding/Tries Implementation/TrieNode.h
@@ -0,0 +1,10 @@
+class TrieNode {
+    char data;
+    TrieNode** children;
+    bool isTerminal;
+    TrieNode(char data){
+        this->data = data;
+        children = new TrieNode*[26];
+        isTerminal = false;
+    }
+};
