b77a022bafb11020bd4e5c072c7fa115a7aefea2
Technical_Rithm <50729418+Madhuvrat1122@users.noreply.github.com>
2024-08-24 17:54:37 +0530

    Update main.cpp

--- Tries and Huffman Coding/Tries Implementation/main.cpp
+++ Tries and Huffman Coding/Tries Implementation/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include "TrieNode.h"
+#include "Trie.h"
 using namespace std;
 
 int main()
 {
+    Trie t;
     
-
+    t.insertWord("Alok");
+    t.insertWord("AlokA");
+    t.insertWord("AlokB");
+    
+    
+    cout<<t.searchWord("AlokB")<<endl; // 1
+    
+    t.removeWord("AlokB");
+    
+    cout<<t.searchWord("AlokB")<<endl; // 0
+    
     return 0;
 }
