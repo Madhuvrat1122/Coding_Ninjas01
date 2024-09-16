d33615f90f61a06a84fb194105a5091dd5cc5cd6
Technical_Rithm <50729418+Madhuvrat1122@users.noreply.github.com>
2024-09-16 23:10:12 +0530

    Update N_Queens.cpp

--- BackTracking/N_Queens.cpp
+++ BackTracking/N_Queens.cpp
@@ -1,47 +1,42 @@
 // Problem statement
-// Given a source point (sx, sy) and a destination point (dx, dy), the task is to check if it is possible to reach the destination point using the following valid moves:
+// You are given an integer 'N'. For a given 'N' x 'N' chessboard, find a way to place 'N' queens such that no queen can attack any other queen on the chessboard.
 
-// (a, b) -> (a + b, b)
-// (a, b) -> (a, a + b)
-// Your task is to find if it is possible to reach the destination point using only the desired moves or not.
+// A queen can be killed when it lies in the same row, or same column, or the same diagonal of any of the other queens. You have to print all such configurations.
 
-// For example:
-// For the coordinates, source point = (1, 1) and destination point = (3, 5)
-// The output will be true as the destination point can be reached using the following sequence of moves:
-// (1, 1) -> (1, 2) -> (3, 2) -> (3, 5)
 // Detailed explanation ( Input/output format, Notes, Images )
 // Constraints:
-// 1 <= T <= 100
-// 1 <= x, y <= 3000
-
-// Where ‘T’ is the number of test cases and ‘x’, ‘y’ are the coordinates of the given points.
+// 1 <= 'N' <= 10
 
 // Time Limit: 1sec
-// Sample Input 1:
-// 2
-// 1 1 3 5
-// 1 1 1 4
-// Sample Output 1:
-// True
-// True
-// Explanation For Sample Input 1:
-// For the first test case
-// The output will be true as destination point can be reached using the following sequence of moves:
-// (1, 1) -> (1, 2) -> (3, 2) -> (3, 5)
 
-// For the second test case
-// The output will be true as destination point can be reached using the following sequence of moves:
-// (1, 1) -> (1, 2) -> (1, 3) -> (1, 4)
+
+// For Example:
+// For a chessboard of size 4*4
+// The configurations are 
+// alt text
+
+// Sample Input 1:
+// 4
+// Sample Output 1:
+// 0 0 1 0 1 0 0 0 0 0 0 1 0 1 0 0
+// 0 1 0 0 0 0 0 1 1 0 0 0 0 0 1 0 
+// Explanation For Sample Input 1:
+// Output depicts two possible configurations of the chessboard for 4 queens.
+
+// The Chessboard matrix for the first configuration looks as follows:-
+
+// 0 0 1 0
+// 1 0 0 0
+// 0 0 0 1
+// 0 1 0 0
+
+// Queen contained cell is depicted by 1. As seen, No queen is in the same row, column, or diagonal as the other queens. Hence this is a valid configuration.
 // Sample Input 2:
-// 2
-// 1 1 2 2
-// 1 1 1 1
-// Sample Output 2:
-// False
-// True
-// bool reachDestination(int sx, int sy, int dx, int dy) {
-//     // Write your code here
-// }
+// 3
+// Sample Output2:
+//       (Blank)
+// Explanation For Sample Input 2:
+// Since no possible configuration exists for 3 Queen's.The output remains Empty.
 
 #include <iostream>
 #include <vector>
