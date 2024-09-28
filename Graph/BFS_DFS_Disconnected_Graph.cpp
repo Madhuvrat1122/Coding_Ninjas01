8ad75046395dd44e28fc4d2a08fc831764b7bd68
Technical_Rithm <50729418+Madhuvrat1122@users.noreply.github.com>
2024-09-28 23:10:37 +0530

    Create BFS_DFS_Disconnected_Graph.cpp

---
+++ Graph/BFS_DFS_Disconnected_Graph.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+// DFS
+void printDFS(int n, int **edges, int startVetice, bool *visited){
+    cout<<startVetice<<" ";
+    visited[startVetice] = true;
+    
+    for(int i=0; i<n; i++){
+        if(i == startVetice){
+            continue;
+        }
+        if(edges[startVetice][i] == 1){
+            if(visited[i]) continue;
+            printDFS(n, edges, i, visited);
+        }
+    }
+}
+void DFS(int **edges, int n){
+    bool *visited = new bool[n];
+    for(int i=0; i<n; i++){
+        visited[i] = false;
+    }
+    
+    for(int i=0; i<n; i++){
+        if(!visited[i]){
+            printDFS(n, edges, i, visited);
+        }
+    }
+    
+    // delete 
+    delete [] visited;
+}
+// BFS
+void printBFS(int n, int **edges, int sv, bool *visited){
+    queue<int> pendingVertices; // push the visited not explored yet
+    
+    pendingVertices.push(sv); // push the current vertex
+    visited[sv] = true; // mark the currebt vertes as visited
+    
+    while(!pendingVertices.empty()){
+        int currentVertex = pendingVertices.front(); // top vertex
+        pendingVertices.pop();
+        cout<<currentVertex<<" "; // print the current vertex
+        // now push the neighbours of current vectex in the queue
+        for(int i=0; i<n; i++){
+            // avoid current
+            if(i == currentVertex) continue;
+            // push the neighbour if not visited yet
+            if(edges[currentVertex][i] == 1 && !visited[i]) {
+                pendingVertices.push(i);
+                 visited[i] = true;
+            }
+        }
+    }
+}
+void BFS(int **edges, int n) {
+    bool *visited = new bool[n];
+    for(int i=0; i<n; i++){
+        visited[i] = false;
+    }
+    
+    for(int i=0; i<n; i++){
+        if(!visited[i]){
+            printBFS(n, edges, i, visited);
+        }
+    }
+    
+    // delete 
+    delete [] visited;
+}
+int main()
+{
+    int n, e;
+    cin>>n>>e;
+    
+    int **edges = new int*[n];
+    for(int i=0; i<n; i++){
+        edges[i] = new int[n];
+        for(int j=0; j<n; j++){
+            edges[i][j] = 0;
+        }
+    }
+    
+    // marking edges in 2-d matrix
+    int s,f;
+    for(int i=0; i<e; i++){
+        cin>>s>>f;
+        edges[s][f] = 1;
+        edges[f][s] = 1;
+    }
+    
+    //BFS
+    cout<<"BFS:-"<<endl;
+    BFS(edges, n);
+    
+    //DFS
+    cout<<endl;
+    cout<<"DFS:-"<<endl;
+    DFS(edges, n);
+        
+    //delete
+    for(int i=0; i<n; i++){
+        delete[] edges[i];
+    }
+
+    return 0;
+}
