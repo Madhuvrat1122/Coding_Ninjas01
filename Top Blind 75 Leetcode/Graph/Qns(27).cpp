169ca34ea46a400f56312c6da79c47bb5401daf0
Technical_Rithm <50729418+Madhuvrat1122@users.noreply.github.com>
2024-11-19 18:35:02 +0530

    Update Qns(27).cpp

--- Top Blind 75 Leetcode/Graph/Qns(27).cpp
+++ Top Blind 75 Leetcode/Graph/Qns(27).cpp
@@ -1,1 +1,50 @@
+// Problem Url:- 
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
 
+class Solution {
+public:
+    void add(int x, unordered_map<int, int>& parent, unordered_map<int, int>& setSize) {
+        if(parent.find(x) == parent.end()){
+            parent[x] = x;
+            setSize[x] = 1;
+        }
+    }
+    int find(int x, unordered_map<int, int>& parent){
+        if(parent[x] != x){
+            parent[x] = find(parent[x], parent);
+        }
+        return parent[x];
+    }
+    void unionSet(int x, int y, vector<int> &arr, unordered_map<int, int>& setSize, unordered_map<int, int>& parent){
+        int rootX = find(x, parent);
+        int rootY = find(y, parent);
+        if(rootX != rootY){
+            if(setSize[rootX] < setSize[rootY]){
+                swap(rootX, rootY);
+            }
+            parent[rootY] = rootX;
+            setSize[rootX] += setSize[rootY];
+        }
+    }
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> parent;
+        unordered_map<int, int> setSize;
+        for(int i=0; i<nums.size(); i++){
+            add(nums[i], parent, setSize);
+            if(parent.find(nums[i]-1) != parent.end()){
+                unionSet(nums[i]-1, nums[i], nums, setSize, parent);
+            }
+            if(parent.find(nums[i]+1) != parent.end()){
+                unionSet(nums[i]+1, nums[i], nums, setSize, parent);
+            }
+        }
+
+        int maximumLen = 0;
+        for(auto i : setSize){
+            if(find(i.first, parent) == i.first){
+                maximumLen = max(maximumLen, i.second);
+            }
+        }
+        return maximumLen;
+    }
+};
